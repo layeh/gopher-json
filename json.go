e45b65570c08a97b97a459e0cc04a53c1affab95
Tim Cooper <tim.cooper@layeh.com>
2016-01-18 16:21:21 -0400

    have Loader as a function, not a variable

--- json.go
+++ json.go
@@ -4,22 +4,16 @@ import (
 	"github.com/yuin/gopher-lua"
 )
 
-// Loader is the module loader function.
-var Loader lua.LGFunction
-
-func init() {
-	Loader = load
-}
-
 // Preload adds json to the given Lua state's package.preload table. After it
 // has been preloaded, it can be loaded using require:
 //
 //  local json = require("json")
 func Preload(L *lua.LState) {
-	L.PreloadModule("json", load)
+	L.PreloadModule("json", Loader)
 }
 
-func load(L *lua.LState) int {
+// Loader is the module loader function.
+func Loader(L *lua.LState) int {
 	t := L.NewTable()
 	L.SetFuncs(t, api)
 	L.Push(t)
